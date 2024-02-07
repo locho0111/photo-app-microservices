16cee344694954e5f6ec17c4abb9b11fe3e18148
rick <iossoares1101@gmail.com>
2024-02-07 12:19:03 -0600

    Initialized Account management service

---
+++ photo-app-api-account-management-service/src/main/java/com/rick/photoappapiaccountmanagementservice/controller/AccountManagementController.java
@@ -0,0 +1,15 @@
+package com.rick.photoappapiaccountmanagementservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountManagementController {
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Account Service Working";
+    }
+}
