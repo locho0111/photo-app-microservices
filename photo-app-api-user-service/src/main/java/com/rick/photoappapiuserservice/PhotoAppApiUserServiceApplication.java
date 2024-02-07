2597e6901dbb127b3e049f64a510298446cf6573
rick <iossoares1101@gmail.com>
2024-02-07 11:40:53 -0600

    Initialized USER service

---
+++ photo-app-api-user-service/src/main/java/com/rick/photoappapiuserservice/PhotoAppApiUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rick.photoappapiuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUserServiceApplication.class, args);
+	}
+
+}
---
+++ photo-app-api-user-service/src/test/java/com/rick/photoappapiuserservice/PhotoAppApiUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.photoappapiuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
