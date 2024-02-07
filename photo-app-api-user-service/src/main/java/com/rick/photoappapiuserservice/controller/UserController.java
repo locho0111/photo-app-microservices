d20d25a8d7e8b9a5c66e6fa34ec71f1eaed5a2ac
rick <iossoares1101@gmail.com>
2024-02-07 13:42:48 -0600

    Added Spring cloud gateway and Account management service

--- photo-app-api-user-service/src/main/java/com/rick/photoappapiuserservice/controller/UserController.java
+++ photo-app-api-user-service/src/main/java/com/rick/photoappapiuserservice/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.rick.photoappapiuserservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private Environment env;
 
     @GetMapping("/status/check")
     public String status() {
-        return "Users Working";
+        return "Users Working on port " + env.getProperty("local.server.port");
     }
 }
