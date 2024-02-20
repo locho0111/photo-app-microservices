4746020ffd072eafa7b7dc6dc58ab85b3481587f
rick <iossoares1101@gmail.com>
2024-02-20 14:05:32 -0600

    Added albums microservice

---
+++ photo-app-api-account-management-service/src/test/java/com/rick/photoappapiaccountmanagementservice/PhotoAppApiAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.photoappapiaccountmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
