5d7abdfe489134e5e9a9afc47a91a81cedac96fc
rick <iossoares1101@gmail.com>
2024-02-22 09:52:12 -0600

    Build artifacts

---
+++ photo-app-config-server/src/main/java/com/rick/photoappconfigserver/Securityconfig.java
@@ -0,0 +1,22 @@
+package com.rick.photoappconfigserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Securityconfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests(request -> request
+                .anyRequest()
+                .permitAll());
+        return http.build();
+    }
+}
